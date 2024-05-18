1091ca658b04f335147063c6ecef1b9a6c2774a7
SataRakotoson <rakotosonsata@gmail.com>
2024-05-18 11:00:06 +0300

    config next.config.j

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ module.exports = withImages({
     images: {
       disableStaticImages: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}` : '',
+    // basePath: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}` : '',
     trailingSlash: true,
-    env: {
-        PUBLIC_URL: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}/` : '/',
-        APP_URL: process.env.NODE_ENV === 'production' ? 'https://d-themes.com/react/molla/' : 'http://localhost/'
-    }
+    // env: {
+    //     PUBLIC_URL: process.env.NODE_ENV === 'production' ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}/` : '/',
+    //     APP_URL: process.env.NODE_ENV === 'production' ? 'https://d-themes.com/react/molla/' : 'http://localhost/'
+    // }
 })
 
